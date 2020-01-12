76e1b30bca49abbea5282d7aac51efcc75563656
Andy Fitzgerald <afitzy@gmail.com>
2020-01-12 15:26:15 -0500

    Add missing sudo to create the root crontab

--- config/createCronJob_rebootComputer.sh
+++ config/createCronJob_rebootComputer.sh
@@ -8,5 +8,5 @@ if [ "$doesCronRebootExist" ]; then
   echo "cron reboot job already exists. Not adding job."
 else
   echo "cron reboot job does not exist. Adding job."
-  (sudo crontab -l 2>/dev/null; echo "0 2 * * Mon,Wed,Fri /home/fitzy/scriptsSBC/reboot") | crontab -
+  (sudo crontab -l 2>/dev/null; echo "0 2 * * Mon,Wed,Fri /home/fitzy/scriptsSBC/reboot") | sudo crontab -
 fi
