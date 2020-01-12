365cf694ec90b9c727c0c01aa3cf4d65c910f073
Andy Fitzgerald <afitzy@gmail.com>
2020-01-12 15:33:19 -0500

    List the crontab the right way instead of stupidly greping the crontab files.

--- config/createCronJob_rebootComputer.sh
+++ config/createCronJob_rebootComputer.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 
-crontabDir="/var/spool/cron/crontabs"
-
-doesCronRebootExist=$(sudo grep --no-messages "reboot" "${crontabDir}/{*,.*}" 2>/dev/null)
-if [ "$doesCronRebootExist" ]; then
-  echo "cron reboot job already exists. Not adding job."
-else
+doesCronRebootExist=$(sudo crontab -l 2>/dev/null | grep reboot 2>/dev/null)
+if [ -z "$doesCronRebootExist" ]; then
   echo "cron reboot job does not exist. Adding job."
   (sudo crontab -l 2>/dev/null; echo "0 2 * * Mon,Wed,Fri /home/fitzy/scriptsSBC/reboot") | sudo crontab -
+else
+  echo "cron reboot job already exists. Not adding job."
 fi
