4289069bd8928d515dbba035319981238bbdafe6
Andy Fitzgerald <afitzy@gmail.com>
2023-01-22 09:09:22 -0500

    Add notes about upgrading worlds. Unclear if that has to be done for every version or just particular versions

--- convenience/startMinecraft.sh
+++ convenience/startMinecraft.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Upgrading world
+# First upgrade your world completely on OLD_VERSION using the --forceUpgrade server argument, and then do the same on NEW_VERSION.
+# Ref: https://www.spigotmc.org/threads/bukkit-craftbukkit-spigot-bungeecord-1-14-4-development-builds.369724/
+# pushd ${HOME}/minecraft && screen "${HOME}/java/jdk-17.0.1+12/bin/java" -jar -Xms1024M -Xmx2048M spigot-1.19.3.jar --forceUpgrade --eraseCache nogui
+
 pushd ${HOME}/minecraft
 screen "${HOME}/java/jdk-17.0.1+12/bin/java" -jar \
     -Xms1024M \ # maximum memory allocation pool for a Java Virtual Machine (JVM)
