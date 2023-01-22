99ba35021288bf4bbe3d1ccc738839dd99bcb725
Andy Fitzgerald <afitzy@gmail.com>
2023-01-22 09:11:10 -0500

    Add 1.19.3 notes

--- convenience/startMinecraft.sh
+++ convenience/startMinecraft.sh
@@ -3,6 +3,7 @@
 # Upgrading world
 # First upgrade your world completely on OLD_VERSION using the --forceUpgrade server argument, and then do the same on NEW_VERSION.
 # Ref: https://www.spigotmc.org/threads/bukkit-craftbukkit-spigot-bungeecord-1-14-4-development-builds.369724/
+# Ref: https://www.spigotmc.org/threads/spigot-bungeecord-1-19-3.581396/
 # pushd ${HOME}/minecraft && screen "${HOME}/java/jdk-17.0.1+12/bin/java" -jar -Xms1024M -Xmx2048M spigot-1.19.3.jar --forceUpgrade --eraseCache nogui
 
 pushd ${HOME}/minecraft
