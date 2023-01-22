59f72ae52170ab4b09ce9e2a28eea3d9105e891e
Andy Fitzgerald <afitzy@gmail.com>
2023-01-22 09:06:42 -0500

    Restore java parameters as I don't have 64-bit java set up

--- convenience/startMinecraft.sh
+++ convenience/startMinecraft.sh
@@ -2,7 +2,7 @@
 
 pushd ${HOME}/minecraft
 screen "${HOME}/java/jdk-17.0.1+12/bin/java" -jar \
-    -Xms4G \ # maximum memory allocation pool for a Java Virtual Machine (JVM)
-    -Xmx4G \ # initial memory allocation pool
+    -Xms1024M \ # maximum memory allocation pool for a Java Virtual Machine (JVM)
+    -Xmx2048M \ # initial memory allocation pool
     spigot-1.19.3.jar nogui
 popd
