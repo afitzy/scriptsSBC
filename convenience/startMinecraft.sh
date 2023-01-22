a07f4d8c2018f1ecc21239325b214b65c677e5e4
Andy Fitzgerald <afitzy@gmail.com>
2023-01-21 21:36:33 -0500

    Bump minecraft version

--- convenience/startMinecraft.sh
+++ convenience/startMinecraft.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd ${HOME}/minecraft
-screen "${HOME}/java/jdk-17.0.1+12/bin/java" -jar -Xms512M -Xmx2048M spigot-1.18.jar nogui
+screen "${HOME}/java/jdk-17.0.1+12/bin/java" -jar -Xms512M -Xmx2048M spigot-1.19.3.jar nogui
 popd
