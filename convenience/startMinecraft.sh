3582120a950d86115d298d3b77b60e3b72232e89
Andy Fitzgerald <afitzy@gmail.com>
2023-01-22 08:56:13 -0500

    Comment java parameters

--- convenience/startMinecraft.sh
+++ convenience/startMinecraft.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 pushd ${HOME}/minecraft
-screen "${HOME}/java/jdk-17.0.1+12/bin/java" -jar -Xms512M -Xmx2048M spigot-1.19.3.jar nogui
+screen "${HOME}/java/jdk-17.0.1+12/bin/java" -jar \
+    -Xms512M \ # maximum memory allocation pool for a Java Virtual Machine (JVM)
+    -Xmx2048M \ # initial memory allocation pool
+    spigot-1.19.3.jar nogui
 popd
